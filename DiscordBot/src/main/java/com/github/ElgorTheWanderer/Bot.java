9e7f115f614676042cb8b1d6c6b8d2d0e8a51160
ElgorTheWanderer <sokorenko_89@yahoo.com>
2020-10-10 19:16:42 +0300

    JSON parser error handling.

--- DiscordBot/src/main/java/com/github/ElgorTheWanderer/Bot.java
+++ DiscordBot/src/main/java/com/github/ElgorTheWanderer/Bot.java
@@ -13,9 +13,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.client.HttpClientResponse;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+
 import org.json.*;
 
 import java.util.List;
@@ -38,66 +40,55 @@ public class Bot {
                     User self = event.getSelf();
                     System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
                 });
-
-
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .map(MessageCreateEvent::getMessage)
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
                 .filter(message -> message.getContent().toLowerCase().startsWith("!user"))
-
-
                 .subscribe(Bot::processIncomingMessage);
-
-
-
         client.onDisconnect().block();
-
     }
-
-
-//        public static void main(String[] args) throws MalformedURLException {
-//            Bot test = new Bot();
-//            test.processIncomingMessage("!User Albanian");
-//        }
-
-
 
     //Основной метод.
     public static void processIncomingMessage(Message message) {
 
-        String accountName = getCommandFromMessage(message.getContent());
-        String targetUrl = buildUrl(accountName);
-        System.out.println(targetUrl);
-        PlayerStructure player1 = new PlayerStructure();
+        String accountName = null;
 
-        //http запрос-ответ.
-        String jsonReplyString = HttpClient.create().get().uri(targetUrl).responseContent().aggregate().asString().block();
-        System.out.println(jsonReplyString);
-        System.out.println();
+        try {
+            accountName = getCommandFromMessage(message.getContent());
+            String targetUrl = buildUrl(accountName);
+            System.out.println(targetUrl);
 
-        //Получить нужный объект JSON. Передать данные структуре.
-        parseResultToStructure(jsonReplyString, accountName, player1);
+            //http запрос-ответ.
+            String jsonReplyString = HttpClient.create().get().uri(targetUrl).responseContent().aggregate().asString().block();
+            System.out.println(jsonReplyString);
+            System.out.println();
 
-
-
-        //Склеить и вернут сообщение в чат.
-//Сообщение не отправляется.
-        String s1 = ("Player Id is - " + player1.playerId + "\n");
-        String s2 = ("Player Name is - " + player1.playerName + "\n");
-        int i1 = (player1.playerKillFame);
-        String s3 = ("Player KillFame is - " + i1);
-        System.out.println(s1 + s2 + s3);
-
-        message.getChannel().flatMap(Channel -> Channel.createMessage(s1 + s2 + s3)).subscribe();
+            //Получить нужный объект JSON. Передать данные структуре.
+            PlayerStructure player1 = parseResultToStructure(jsonReplyString, accountName);
+            //Склеить и вернут сообщение в чат.
+            String s1 = ("Player Id is - " + player1.playerId + "\n");
+            String s2 = ("Player Name is - " + player1.playerName + "\n");
+            int i1 = (player1.playerKillFame);
+            String s3 = ("Player KillFame is - " + i1);
+            System.out.println(s1 + s2 + s3);
+            message.getChannel().flatMap(Channel -> Channel.createMessage(s1 + s2 + s3)).subscribe();
+        } catch (Exception e) {
+            e.printStackTrace();
+            String s = "Error: " + e.getMessage();
+            message.getChannel().flatMap(Channel -> Channel.createMessage(s)).subscribe();
         }
 
+
+
+    }
+
     //Отрезать команду от имени.
-    public static String getCommandFromMessage(String messageContent){
+    public static String getCommandFromMessage(String messageContent) {
         if (messageContent.length() <= 5) {
             return null;
         }
-            String temp = messageContent.substring(6);
-            return temp;
+        String temp = messageContent.substring(6);
+        return temp;
     }
 
     //Склеить адрес для запроса.
@@ -107,33 +98,25 @@ public class Bot {
     }
 
     //Получить объект JSON с нужным именем игрока. Передать данные структуре.
-    public static void parseResultToStructure(String JSONString, String name, PlayerStructure player) {
-            JSONArray arr = new JSONObject(JSONString).getJSONArray("players");
-            int i;
-            for(i = 0; i < arr.length(); i++){
-                if(arr.getJSONObject(i).getString("Name").equals(name)){
-                    break;
-                }
+    public static PlayerStructure parseResultToStructure(String JSONString, String name) throws Exception {
+        JSONArray arr = new JSONObject(JSONString).getJSONArray("players");
+
+        PlayerStructure player = null;
+        for (int i = 0; i < arr.length(); i++) {
+            if (arr.getJSONObject(i).getString("Name").equals(name)) {
+                JSONObject jo;
+                jo = arr.getJSONObject(i);
+                player = new PlayerStructure();
+                player.playerId = jo.getString("Id");
+                player.playerName = jo.getString("Name");
+                player.playerKillFame = jo.getInt("KillFame");
+                break;
             }
-            JSONObject jo;
-            jo = arr.getJSONObject(i);
-            player.playerId = jo.getString("Id");
-            player.playerName = jo.getString("Name");
-            player.playerKillFame = jo.getInt("KillFame");
+        }
+        if (player == null){
+         throw new Exception("User not found");
+        }
 
-
-
-
-
-
-
-            //        JSONObject info = new JSONObject(JSONString).getJSONArray("players").getJSONObject(0);
-//        System.out.println(info.getString("Name"));
-//        player.playerId = info.getString("Id");
-//        player.playerName = info.getString("Name");
-//        player.playerKillFame = info.getInt("KillFame");
-
-
+        return player;
     }
-
 }
